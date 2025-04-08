7e9ab1522163600092d5aaf64e820f585fcb94ee
Jubayer Isfar <isfarjubayer@gmail.com>
2025-04-09 01:41:53 +0600

    removing xgboost as it costs memory

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, render_template, request
 import numpy as np
 import joblib
-import xgboost as xgb
-import keras
 import ydf
 from tensorflow.keras.models import load_model
 import pandas as pd
@@ -10,8 +8,6 @@ import os
 # Load models and data
 rf_model = joblib.load('models/rf_model.pkl')  # Random Forest
 svm_model = joblib.load('models/svm_model.pkl')  # SVM
-xgb_model = xgb.Booster()  # XGBoost
-xgb_model.load_model('models/xgb_model.json')  # Load XGBoost model
 ydf_model = ydf.load_model('models/ydf_model')  # YDF
 ann_model = load_model("models/ann_best_model.keras")
 X = joblib.load("X_columns.pkl")  # Feature columns
@@ -37,9 +33,9 @@ def prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Sui
     # Create a dictionary with the features and their values
     input_data = {X.columns[i]: values[i] for i in range(len(X.columns))}
 
-    if isinstance(model, xgb.Booster):
-        dmatrix = xgb.DMatrix(values.reshape(1, -1), feature_names=X.columns.tolist())
-        return int(model.predict(dmatrix)[0] >= 0.5)  
+    # if isinstance(model, xgb.Booster):
+    #     dmatrix = xgb.DMatrix(values.reshape(1, -1), feature_names=X.columns.tolist())
+    #     return int(model.predict(dmatrix)[0] >= 0.5)  
 
     if isinstance(model, ydf.model.generic_model.GenericModel):
         input_df = pd.DataFrame([input_data])
@@ -73,10 +69,10 @@ def index():
         ann_pred = prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Suicide, Study, Financial, History, Subject, ann_model)
         rf_pred = prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Suicide, Study, Financial, History, Subject, rf_model)
         svm_pred = prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Suicide, Study, Financial, History, Subject, svm_model)
-        xgb_pred = prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Suicide, Study, Financial, History, Subject, xgb_model)
+
         ydf_pred = prediction_ml(Gender, Age, Pressure, CGPA, Satisfaction, Sleep, Dietary, Suicide, Study, Financial, History, Subject, ydf_model)
 
-        return render_template('index.html', ann_pred=ann_pred, rf_pred=rf_pred, svm_pred=svm_pred, xgb_pred=xgb_pred, ydf_pred=ydf_pred)
+        return render_template('index.html', ann_pred=ann_pred, rf_pred=rf_pred, svm_pred=svm_pred, ydf_pred=ydf_pred)
 
     return render_template('index.html')
 
