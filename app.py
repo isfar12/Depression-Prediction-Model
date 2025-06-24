233995b59f7ee39a7b7e4fe1b44af48d8bc8885b
Jubayer Isfar <isfarjubayer@gmail.com>
2025-06-24 14:46:53 +0600

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from tensorflow.keras.models import load_model
 import pandas as pd
 import os
 # Load models and data
-# rf_model = joblib.load('models/rf_model.pkl')  # Random Forest
-# svm_model = joblib.load('models/svm_model.pkl')  # SVM
-# ydf_model = ydf.load_model('models/ydf_model')  # YDF
-# ann_model = load_model("models/ann_best_model.keras")
+rf_model = joblib.load('models/rf_model.pkl')  # Random Forest
+svm_model = joblib.load('models/svm_model.pkl')  # SVM
+ydf_model = ydf.load_model('models/ydf_model')  # YDF
+ann_model = load_model("models/ann_best_model.keras")
 X = joblib.load("X_columns.pkl")  # Feature columns
 
 # Prediction function
